b23b84dd0386d604bfd334d8753244f3e587d065
Cameron Moorehead <hello@cameronmoorehead.com>
2018-02-07 13:51:25 -0800

    basic socketio artillery load test for sockets

--- __test__/socket.test.js
+++ __test__/socket.test.js
@@ -4,36 +4,23 @@ require('./lib/setup');
 const { start, stop } = require('../src/lib/server');
 const io = require('socket.io-client');
 const __API_URL__ = `http://localhost:${process.env.PORT}`;
-let ioOptions = { 
+let ioOptions = {
   transports: ['websocket'],
   forceNew: true,
   reconnection: false,
   multiplex: false,
 };
-// let ioOptions = {};
+
 let sender, receiver, p3;
 
 describe('socket.io', () => {
   beforeEach(start);
   afterEach(stop);
-  
+
   describe('player updates', () => {
     test('emitted player object should be received', (done) => {
-      // let playerObject = {
-      //   direction: 'right',
-      //   characterStatus: [1,2,3,4,5,6,7,8],
-      // };
-      // let badObject = {
-      //   direction: 'left',
-      //   characterStatus: [1,2],
-      // };
-      // console.log('sender', sender);
-      // console.log('receiver', receiver);
-
       sender = io(__API_URL__, ioOptions);
-      
-      
-      
+
       sender.on('connection', () => {
         console.log('sender connected');
         receiver = io(__API_URL__, ioOptions);
@@ -60,14 +47,6 @@ describe('socket.io', () => {
           });
         });
       });
-       
-      // sender.emit('update-player', playerObject);
-      // receiver.on('render-players', function(msg) {
-      //   console.log(msg);
-      //   expect(msg).toEqual(badObject);
-      //   expect(1).toEqual(2);
-      // });
-      // done();
     });
   });
 });
