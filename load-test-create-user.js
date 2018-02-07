ecd50ecc92b9e1a4903b36b979618dc66ca812f5
jjkusowski <jjkusowski@yahoo.com>
2018-02-07 11:15:03 -0800

    account /signup load tested

---
+++ load-test-create-user.js
@@ -0,0 +1,16 @@
+'use strict';
+const faker = require('faker');
+module.exports = {generateRandomAccount};
+
+function generateRandomAccount(userContext, events, done) {
+  userContext.vars.username = faker.internet.userName();
+  userContext.vars.email = faker.internet.email();
+  userContext.vars.password = faker.internet.password();
+
+  // userContext.vars.bio = faker.lorem.words(10);
+  // userContext.vars.avatar = faker.image.imageUrl();
+  // userContext.vars.firstName = faker.name.firstName();
+  // userContext.vars.lastName = faker.name.lastName();
+
+  return done();
+}
