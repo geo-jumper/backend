42cd4cdd913a9f3b0fc0536e82c3c8602bd4191c
Snobeard <fatmattmatt@gmail.com>
2018-02-08 15:35:23 -0800

    removing logs, preventing mongoose breaking

--- src/model/high-score.js
+++ src/model/high-score.js
@@ -25,30 +25,31 @@ HighScore.create = function(newScore) {
 };
 
 HighScore.update = function(newScore) {
+  if (isNaN(newScore.level)) {
+    return;
+  }
   let tempNewScore = newScore;
   console.log('newScore', newScore);
   const NUMSCORES = 20; //Jeff - Number of scores to save in db
   let options = { new: true, runValidators: true };
   return HighScore.find({'level': newScore.level}).sort({'scores.score': 'desc'})
     .then(scoreObj => {
-      console.log('scoreObj: ', scoreObj);
       if(!scoreObj.length) {
         return HighScore.create(tempNewScore);
       } else {
         if(scoreObj[0].scores.length < NUMSCORES) {
-          console.log('score object length: ', scoreObj[0].scores.length);
           //Jeff - add the new score.  Not important to be sorted.  Will sort when extracting for GET request.
           scoreObj[0].scores.push({score:tempNewScore.score, username: tempNewScore.username});
-          console.log('updated scoreObj[0].scores: ', scoreObj[0].scores);
-          console.log('new score object length: ', scoreObj[0].scores.length );
           //Jeff - update the db
-          console.log('tempNewScore.level: ', tempNewScore.level);
           HighScore.findOneAndUpdate({level: tempNewScore.level }, {scores: scoreObj[0].scores}, options, (error, data) => {
+            if (error) {
+              console.log(error);
+            }
             console.log(data);
           });
         } else {
           //Jeff - compare to last score in array
-          if(tempNewScore.score > scoreObj.scores[NUMSCORES - 1]){
+          if(tempNewScore.score > scoreObj[0].scores[NUMSCORES - 1]){
             //Jeff - if bigger, replace.  Not important to be sorted.  Will sort when extracting for GET request.
             scoreObj[0].scores.splice(NUMSCORES - 1, 1, {
               score: tempNewScore.score,
