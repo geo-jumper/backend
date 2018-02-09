c45ee539d03e5b7a49e51ce474ddfb7425a8aa7f
jjkusowski <jjkusowski@yahoo.com>
2018-02-08 16:37:25 -0800

    added callback to update

--- src/model/high-score.js
+++ src/model/high-score.js
@@ -52,14 +52,19 @@ HighScore.update = function(newScore) {
           console.log('if tempNewScore.score', tempNewScore.score, '> scoreObj[0].scores[19].score', scoreObj[0].scores[NUMSCORES - 1].score);
           if(tempNewScore.score > scoreObj[0].scores[NUMSCORES - 1].score){
             //Jeff - if bigger, replace.  Not important to be sorted.  Will sort when extracting for GET request.
-            console.log('update scores: old scoreObj[0].scores');
+            console.log('update scores: old', scoreObj[0].scores);
             scoreObj[0].scores.splice(NUMSCORES - 1, 1, {
               score: tempNewScore.score,
               username: tempNewScore.username,
             });
-            console.log('update scores: new scoreObj[0].scores');
+            console.log('update scores: new ', scoreObj[0].scores);
             //Jeff - update the db
-            HighScore.findOneAndUpdate({ level: tempNewScore.level }, { scores: scoreObj[0].scores }, options);
+            HighScore.findOneAndUpdate({ level: tempNewScore.level }, { scores: scoreObj[0].scores }, options, (error, data) => {
+              if(error) {
+                console.log(error);
+              }
+              console.log(data);
+            });
           }
           //Jeff - else do nothing
         }
